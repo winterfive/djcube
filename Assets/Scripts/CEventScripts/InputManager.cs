407a65c3a49957140d7de0b2948994c649554d7f
winterfive <piealacode@gmail.com>
2018-11-05 16:01:27 -0500

    Created InputManager class

---
+++ Assets/Scripts/CEventScripts/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class InputManager : SetAsSingleton<InputManager>
+{
+    // Manages all player input
+
+    public delegate void Click();
+    public static event Click OnClick;
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (OnClick != null)
+            {
+                OnClick();
+            }
+        }
+    }
+}
